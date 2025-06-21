2ba0334a51576260f417da09424b624e380cab07
Swarup <mankar04@gmail.com>
2025-06-21 14:42:50 +0530

    changeee--

--- src/routes/thumbnail.routes.js
+++ src/routes/thumbnail.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const thumbnailController = require("../controllers/thumbnail.controller");
 const auth = require("../middleware/auth.middleware");
 
