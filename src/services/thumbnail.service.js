5829d5fde6ad224ba6c06f087bc668db53bde47f
Swarup <mankar04@gmail.com>
2025-06-21 14:15:48 +0530

    some thhumbnail changes

--- src/services/thumbnail.service.js
+++ src/services/thumbnail.service.js
@@ -6,8 +6,8 @@ async function fetchThumbnail(url) {
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114 Safari/537.36",
+      Referer: "",
     },
-    timeout: 7000,
   });
 
   const html = response.data;
